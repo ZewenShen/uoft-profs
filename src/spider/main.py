fa2a8fd452ad616859f7ed48ff24709db01b6ff4
Walden-Shen <41880356@qq.com>
2018-05-13 19:55:56 +0800

    get json of all the courses data

--- src/spider/main.py
+++ src/spider/main.py
@@ -1,15 +1,36 @@
-import ConnectDatabase
+import Database
 import json
 import requests
 from urllib.parse import urlencode
 
-BASE_URL = 'http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch?'
+BASE_URL = 'http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/search?'
 
 headers = {
-    'Referer': 'http://coursefinder.utoronto.ca/course-search/search/courseSearch/course/browseSearch?viewId=CourseSearch-FormView&methodToCall=start',
+    'Referer': 'http://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
 }
+
+all_courses = 'queryText=&requirements=&campusParam=St.%20George,Scarborough,Mississaga'
+
+all_courses_params = {
+    'queryText': '',
+    'requirements': '',
+    'campusParam': 'St. George,Scarborough,Mississaga'
+}
+
+def get_all_courses():
+    url = BASE_URL + urlencode(all_courses_params)
+    session = requests.session()
+    try:
+        session.get(url, headers=headers) # don't delete this line. It enables you to get cookies, otherwise you will get 403
+        r = session.get(url, headers=headers) 
+        if r.status_code == 200:
+            return r.json()
+    except requests.ConnectionError as e:
+        print('Error', e.args)
+        return None
+
 
 def get_courses(deptId, divId):
     params = {
