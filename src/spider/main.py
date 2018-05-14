fda368139c93106ccf41e40192f4a0b4e0d9938f
Walden-Shen <41880356@qq.com>
2018-05-14 10:43:23 +0800

    debug for spider

--- src/spider/main.py
+++ src/spider/main.py
@@ -33,41 +33,40 @@ def get_all_courses_json():
         return None
 
 def parse_json(json):
-    for page in json['aaData']:
-        for course in page:
-            detail_url = re.search(".*?coursedetails/(.*?)'>", course[1]).group(1)
-            url = DETAIL_BASE_URL + detail_url
-            info_dict = parse_course_detail(get_course_detail(url))
+    for course in json['aaData']:
+        detail_url = re.search("coursedetails/(.*?)'>", course[1]).group(1)
+        url = DETAIL_BASE_URL + detail_url
+        info_dict = parse_course_detail(get_course_detail(url))
 
-            info_dict['cID'] = re.search(">(.*?)<", course[1]).group(1)
-            info_dict['cName'] = course[2]
-            info_dict['credits'] = course[3]
-            info_dict['campus'] = course[4].strip(' ')
-            info_dict['department'] = course[5].strip(' ')
-            info_dict['term'] = course[6]
-            info_dict['division'] = course[7].strip(' ')
-
+        info_dict['cID'] = re.search(">(.*?)<", course[1]).group(1)
+        info_dict['cName'] = course[2]
+        info_dict['credits'] = course[3]
+        info_dict['campus'] = course[4].strip(' ')
+        info_dict['department'] = course[5].strip(' ')
+        info_dict['term'] = course[6]
+        info_dict['division'] = course[7].strip(' ')
+        print(info_dict)
 
 def get_course_detail(url):
     try:
         r = requests.get(url, headers = headers)
         if r.status_code == 200:
-            return parse_course_detail(r.text)
+            return r.text
     except requests.ConnectionError as e:
         print('Error', e.args)
         return None
 
 def parse_course_detail(html):
     soup = BeautifulSoup(html, 'lxml')
-    prerequisites = soup.find_all('span', attrs = {'id': "u50"})
-    exclusion = soup.find_all('span', attrs = {'id': 'u68'})
-    br = soup.find_all('span', attrs = {'id': 'u122'})
+    prerequisites = soup.find('span', attrs = {'id': "u50"})
+    exclusion = soup.find('span', attrs = {'id': 'u68'})
+    br = soup.find('span', attrs = {'id': 'u122'})
     table = soup.find_all('span', attrs = {'id': re.compile("^u2(45|54|63|72|81|90)_line\d$")})
     clean_pattern = re.compile(' \n|\n|\r')
     return {
-            "prerequisites": [item.getText().strip('\r\n') for item in prerequisites],
-            "exclusion": [item.getText().strip('\r\n') for item in exclusion],
-            "br": [item.getText().strip('\r\n') for item in br],
+            "prerequisites": prerequisites.getText().strip('\r\n') if prerequisites is not None else None,
+            "exclusion": exclusion.getText().strip('\r\n') if exclusion is not None else None,
+            "br": br.getText().strip('\r\n') if br is not None else None,
             "lecNum": [re.sub(clean_pattern, '', table[6 * i].getText()) for i in range(len(table) // 6)],
             "lecTime": [re.sub(clean_pattern, '', table[1 + 6 * i].getText()) for i in range(len(table) // 6)],
             "instructor": [re.sub(clean_pattern, '', table[2 + 6 * i].getText()) for i in range(len(table) // 6)],
