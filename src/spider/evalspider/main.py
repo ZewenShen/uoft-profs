dda00411c42e761f3af07fc50cf281501a20a9d0
Walden-Shen <41880356@qq.com>
2018-05-15 14:30:53 +0800

    weak version evalspider done

--- src/spider/evalspider/main.py
+++ src/spider/evalspider/main.py
@@ -51,22 +51,36 @@ def get_course_evals(pageActuelle):
         print('Error', e.args)
 
 def clean_course_evals(xml_string):
-    tree = fromstring(xml_string)
-    eval_data_in_xml = tree[0]
+    tree = fromstring(xml_string) # turn the xmlstring into real xml structure
+    eval_data_in_xml = tree[0] # the eval data we need is in index 0
     soup = BeautifulSoup(eval_data_in_xml.text, 'lxml')
     
+    tr_taglist = soup.find_all('tr', attrs={'class': 'gData'})
     taglist = soup.find_all('td')
 
-    clean_pattern = re.compile('<td.*?>|</td>')
-
-    lecNum = "Lec " + re.search("-LEC(\d{4})$", taglist[1].getText()).group(1)
+    uncleaned_course_info = taglist[1].getText() # e.g., 'Human Embroyology ANA301H1-SLEC0101'
+    __split_index = uncleaned_course_info.rfind(' ') + 1
+    uncleaned_courseID = uncleaned_course_info[__split_index:] # e.g., "ANA201H1-S-LEC0101"
+    lecNum = "Lec " + re.search("-LEC(\d{4})$", uncleaned_courseID).group(1) # use 'Lec' instead of 'LEC' here to sync with Course database table
+    cID = uncleaned_courseID.split('-')[0]
+    season = uncleaned_courseID.split('-')[1]
+    cName = uncleaned_course_info[: __split_index]
 
     return {
         "department": taglist[0].getText(),
-        #"cID": ,
+        "cID": cID,
+        "cName": cName,
         "lecNum": lecNum, 
         "campus": "St. George",
-        "instructor": "{} {}".format(taglist[3], taglist[2]),
+        "term": "{} {} {}".format(taglist[5].getText(), taglist[4].getText(), season),
+        "instructor": "{} {}".format(taglist[3].getText()[0], taglist[2].getText()), # the instructor column in Course database table only have initial first name instead of full name
+        "instructorFullName": "{} {}".format(taglist[3].getText(), taglist[2].getText()),
+        "intellectuallySimulating": taglist[6].getText(),
+        "deeperUnderstanding": taglist[7].getText(),
+        "courseAtmosphere": taglist[8].getText(),
+        "homeworkQuality": taglist[9].getText(),
+        "homeworkFairness": taglist[10].getText(),
+        "overallQuality": taglist[11].getText(),
         "enthusiasm": taglist[12].getText(),
         "workload": taglist[13].getText(),
         "recommend": taglist[14].getText(),
@@ -75,6 +89,36 @@ def clean_course_evals(xml_string):
     }
 
 
+def extract_one_course(tr_taglist):
+    td_taglist = tr_taglist.find_all('td')
 
+    uncleaned_course_info = td_taglist[1].getText() # e.g., 'Human Embroyology ANA301H1-SLEC0101'
+    __split_index = uncleaned_course_info.rfind(' ') + 1
+    uncleaned_courseID = uncleaned_course_info[__split_index:] # e.g., "ANA201H1-S-LEC0101"
+    lecNum = "Lec " + re.search("-LEC(\d{4})$", uncleaned_courseID).group(1) # use 'Lec' instead of 'LEC' here to sync with Course database table
+    cID = uncleaned_courseID.split('-')[0]
+    season = uncleaned_courseID.split('-')[1]
+    cName = uncleaned_course_info[: __split_index]
 
+    return {
+        "department": taglist[0].getText(),
+        "cID": cID,
+        "cName": cName,
+        "lecNum": lecNum, 
+        "campus": "St. George",
+        "term": "{} {} {}".format(td_taglist[5].getText(), td_taglist[4].getText(), season),
+        "instructor": "{} {}".format(td_taglist[3].getText()[0], td_taglist[2].getText()), # the instructor column in Course database table only have initial first name instead of full name
+        "instructorFullName": "{} {}".format(td_taglist[3].getText(), td_taglist[2].getText()),
+        "intellectuallySimulating": td_taglist[6].getText(),
+        "deeperUnderstanding": td_taglist[7].getText(),
+        "courseAtmosphere": td_taglist[8].getText(),
+        "homeworkQuality": td_taglist[9].getText(),
+        "homeworkFairness": td_taglist[10].getText(),
+        "overallQuality": td_taglist[11].getText(),
+        "enthusiasm": td_taglist[12].getText(),
+        "workload": td_taglist[13].getText(),
+        "recommend": td_taglist[14].getText(),
+        "numInvited": td_taglist[15].getText(),
+        "numResponded": td_taglist[16].getText()
+    }
 
